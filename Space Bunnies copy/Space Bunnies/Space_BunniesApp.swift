9f03f9463a02005ab3d0aebb0a906117424a89b0
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:01:32 -0400

    Add Space Bunnies copy/Space Bunnies/Space_BunniesApp.swift

---
+++ Space Bunnies copy/Space Bunnies/Space_BunniesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_BunniesApp.swift
+//  Space Bunnies
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct Space_BunniesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
